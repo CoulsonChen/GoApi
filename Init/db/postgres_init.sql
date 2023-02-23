3468dd8c2e6c7871eae703f7177587724fb69b46
CoulsonChen <a0936792910@gmail.com>
2023-02-23 23:20:44 +0800

    Ｃommit 3: Create a new script file to populate an users table.

---
+++ Init/db/postgres_init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users( 
+    acct varchar(20) NOT NULL, 
+    pwd varchar(20) NOT NULL, 
+    fullname varchar(50) NOT NULL, 
+    created_at timestamptz NOT NULL DEFAULT (now()), 
+    updated_at timestamptz NOT NULL DEFAULT (now()),
+    CONSTRAINT PK_users PRIMARY KEY (acct)
+);
